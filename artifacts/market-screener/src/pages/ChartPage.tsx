31b813f44627d94e140ee34e80cde52ca0623fd1
Divya Shekhar <asutoshkhanna354@gmail.com>
2026-06-25 15:30:32 +0530

    removed bad files

--- artifacts/market-screener/src/pages/ChartPage.tsx
+++
@@ -1,9 +0,0 @@
-import { TradingChart } from "@/components/TradingChart";
-
-export function ChartPage() {
-  return (
-    <div className="flex-1 overflow-hidden">
-      <TradingChart />
-    </div>
-  );
-}
